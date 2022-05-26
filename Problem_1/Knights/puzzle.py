29177a8b93e63f2ea88022c09522533ccfe7763c
Henrylau127 <valve.9527@gmail.com>
2022-05-26 19:06:49 +0800

    Completed Knowledge 0 and 1

--- Problem_1/Knights/puzzle.py
+++ Problem_1/Knights/puzzle.py
@@ -9,24 +9,50 @@ BKnave = Symbol("B is a Knave")
 CKnight = Symbol("C is a Knight")
 CKnave = Symbol("C is a Knave")
 
+# Knave always lies = false
+# Knight always speaks the truth = true
+
 # Puzzle 0
 # A says "I am both a knight and a knave."
+# Expected: A is a Knave
 knowledge0 = And(
-    # TODO
+    # Conditions given by the puzzle
+    Or(AKnight, AKnave),
+    Not(And(AKnight, AKnave)),
+
+    # A cannot be both a knight and a knave at the same time
+    # Therefore, he is lying and therefore a knave
+    Implication(AKnight, AKnave)
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
+# Expected: A is a Knave
+#           B is a Knight
 knowledge1 = And(
-    # TODO
+    # Conditions given by the puzzle
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
+
+    # A claims both of them are knaves,
+    # but he is clearly lying as B did not confirm or deny anything
+    Biconditional(AKnight, And(AKnave, BKnave))
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
+# Expected: A is a Knave
+#           B is a Knight
 knowledge2 = And(
-    # TODO
+    # Conditions given by the puzzle
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
 )
 
 # Puzzle 3
@@ -34,8 +60,17 @@ knowledge2 = And(
 # B says "A said 'I am a knave'."
 # B says "C is a knave."
 # C says "A is a knight."
+# Expected: A is a knight
+#           B is a knave
+#           C is a knight
 knowledge3 = And(
-    # TODO
+    # Conditions given by the puzzle
+    Or(AKnight, AKnave),
+    Or(BKnight, BKnave),
+    Or(CKnight, CKnave),
+    Not(And(AKnight, AKnave)),
+    Not(And(BKnight, BKnave)),
+    Not(And(CKnight, CKnave)),
 )
 
 
